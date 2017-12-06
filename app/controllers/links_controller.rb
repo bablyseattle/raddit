de1e04578fe341b9e71f6b6ecd6134177df05286
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-12-05 16:03:38 -0800

    auth of links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,6 +1,8 @@
 class LinksController < ApplicationController
   before_action :set_link, only: [:show, :edit, :update, :destroy]
 
+  before_action :authenticate_user!, except:[:index, :show] 
+
   # GET /links
   # GET /links.json
   def index
@@ -14,7 +16,7 @@ class LinksController < ApplicationController
 
   # GET /links/new
   def new
-    @link = Link.new
+    @link = current_user.links.build
   end
 
   # GET /links/1/edit
@@ -24,7 +26,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = Link.new(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
--- db/migrate/20171130224741_add.rb
+++ db/migrate/20171130224741_add.rb
@@ -1,4 +1,5 @@
 class Add < ActiveRecord::Migration[5.1]
   def change
+  	
   end
 end
